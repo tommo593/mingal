9a48494c020534e758ea4d9c620ace96c570c7ba
tommo593 <tomdevwork@gmail.com>
2024-05-05 12:27:57 +0100

    Added header, footer, auth

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href={"/"} className="w-36">
+          <Image src="" width={128} height={38} alt="Mingal logo" />
+        </Link>
+        <div className="flex w-32 justify-end gap-3"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
