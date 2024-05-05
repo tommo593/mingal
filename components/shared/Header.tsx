37b649061e9059972d6a7d7459fd0869cbbda3d0
tommo593 <tomdevwork@gmail.com>
2024-05-05 17:17:19 +0100

    Added more pages

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,10 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -9,7 +13,23 @@ const Header = () => {
         <Link href={"/"} className="w-36">
           <Image src="" width={128} height={38} alt="Mingal logo" />
         </Link>
-        <div className="flex w-32 justify-end gap-3"></div>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
