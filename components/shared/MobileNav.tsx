a307f877361dd9b09e15b80314de8394332a8387
tommo593 <tomdevwork@gmail.com>
2024-05-05 17:56:11 +0100

    Added components to navbar and made it responsive

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+
+import Image from "next/image";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="menu"
+            width={25}
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+          <Separator />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
 };
 
 export default MobileNav;
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NavItems = () => {
-  return <div>NavItems</div>;
+  return (
+    <ul className="md:flex-between flex">
+      <li></li>
+    </ul>
+  );
 };
 
 export default NavItems;
