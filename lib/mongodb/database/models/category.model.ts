d3a3062bb77101557abfdf1aa92f8904368c9f6d
tommo593 <tomdevwork@gmail.com>
2024-05-09 13:56:55 +0100

    Stuff

--- lib/mongodb/database/models/category.model.ts
+++ lib/mongodb/database/models/category.model.ts
@@ -1,5 +1,4 @@
-import { Document } from "mongodb";
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface ICategory extends Document {
   _id: string;
